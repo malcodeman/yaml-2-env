1904170d17a11229149e25c09591fc4a5bc80542
malcodeman <26983729+malcodeman@users.noreply.github.com>
2023-03-21 23:31:36 +0100

    Updates document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
-import Document from "next/document";
 import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 const getInitialProps = createGetInitialProps();
 
 class _Document extends Document {
   static getInitialProps = getInitialProps;
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default _Document;
